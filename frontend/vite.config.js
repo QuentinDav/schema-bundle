f01f75545331da0b951ffd5fe698e9b7f0220ec1
Quentin David <quentindavid@MBP-de-Quentin.lan>
2025-11-04 22:28:31 +0100

    Switch from Tailwind 4 to Tailwind 3 for stability

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/schema-doc/',
   plugins: [
-    tailwindcss(),
     vue(),
     vueJsx(),
   ],
